ec3578ff07e5962e05920a42dbb280080e80c805
samny520 <1318578251@qq.com>
2019-12-13 19:45:42 +0800

    Site updated: 2019-12-13 19:45:41

---
+++ js/picture.js
@@ -0,0 +1,23 @@
+// //生成从minNum到maxNum的随机数
+// (function($){
+//     $.fn.picture = function(minNum,maxNum){ 
+//     switch(arguments.length){ 
+//         case 1: 
+//             return "https://acg.toubiec.cn/random?id=" + (Math.random()*minNum+1,10); 
+//         break; 
+//         case 2: 
+//             return "https://acg.toubiec.cn/random?id=" +parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+//         break; 
+//             default: 
+//                 return "https://acg.toubiec.cn/random"; 
+//             break; 
+//     }; 
+// };
+// })(jQuery);
+// $(function(){
+//     $.fn.picture({
+//         maxNum: 500,
+//         minNum: 1
+
+//     });
+// });
