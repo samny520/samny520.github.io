bb88615c387e145ab1c97f060f9772a9905b7ca5
samny520 <1318578251@qq.com>
2020-03-23 12:52:17 +0800

    Site updated: 2020-03-23 12:52:15

---
+++ js/test.js
@@ -0,0 +1,10 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?eadef2421de71beaad5d2a625cd47181";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+
